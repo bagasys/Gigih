0def75101378a7eaf5f947e63ec3c6ba2b7d1788
bagasys <bagasys3@gmail.com>
2021-07-28 18:47:13 +0700

    add unittest: increment single digit of 9

--- Modul4/Session2/handson/spec/increment_array_spec.rb
+++ Modul4/Session2/handson/spec/increment_array_spec.rb
@@ -1,7 +1,7 @@
 require_relative "../src/array_incrementer"
 
 RSpec.describe ArrayIncrementer do
-  it 'is delicious' do
+  it 'increments single digit' do
     # given
     ai = ArrayIncrementer.new()
 
@@ -10,5 +10,16 @@ RSpec.describe ArrayIncrementer do
 
     # then
     expect(incremented_arr).to eq ([1])
+  end
+
+  it 'increments single digit of 9' do
+    # given
+    ai = ArrayIncrementer.new()
+
+    #when
+    incremented_arr = ai.increment([9])
+
+    # then
+    expect(incremented_arr).to eq ([1, 0])
   end
 end
