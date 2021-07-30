5c1cf1e3337194a44baee1736d2e1bb881c32200
bagasys <bagasys3@gmail.com>
2021-07-30 09:46:59 +0700

    Homework Module 4 Session2: rspec + simplecov

---
+++ Modul4/Session2/Homework/spec/increment_array_spec.rb
@@ -0,0 +1,58 @@
+require_relative "../src/array_incrementer"
+
+RSpec.describe ArrayIncrementer do
+  it 'increments single digit' do
+    # given
+    ai = ArrayIncrementer.new()
+
+    #when
+    incremented_arr = ai.increment([0])
+
+    # then
+    expect(incremented_arr).to eq ([1])
+  end
+
+  it 'increments single digit of 9' do
+    # given
+    ai = ArrayIncrementer.new()
+
+    #when
+    incremented_arr = ai.increment([9])
+
+    # then
+    expect(incremented_arr).to eq ([1, 0])
+  end
+
+  it 'increments two digits' do
+    # given
+    ai = ArrayIncrementer.new()
+
+    #when
+    incremented_arr = ai.increment([1, 1])
+
+    # then
+    expect(incremented_arr).to eq ([1, 2])
+  end
+
+  it 'increments two digits of 9s' do
+    # given
+    ai = ArrayIncrementer.new()
+
+    #when
+    incremented_arr = ai.increment([9, 9])
+
+    # then
+    expect(incremented_arr).to eq ([1, 0, 0])
+  end
+
+  it 'receives empty array' do
+    # given
+    ai = ArrayIncrementer.new()
+
+    #when
+    incremented_arr = ai.increment([])
+
+    # then
+    expect(incremented_arr).to eq ([1])
+  end
+end
---
+++ Modul4/Session2/Homework/src/array_incrementer.rb
@@ -0,0 +1,29 @@
+class ArrayIncrementer
+  def increment(arr)
+    newArr = arr
+    n = arr.length
+
+    if n < 1
+      return [1]
+    end
+
+    i = n - 1
+    sisa = 1
+    
+    while i >= 0 && sisa == 1
+      if newArr[i] == 9
+        newArr[i] = 0
+        sisa = 1
+      else
+        newArr[i] += sisa
+        sisa = 0
+      end
+      i -= 1
+    end
+
+    if sisa == 1
+      newArr = [1] + newArr
+    end
+    newArr
+  end
+end
